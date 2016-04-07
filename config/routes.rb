1264361c8916bc81be1f8a718540d9191e8a75ba
Emma Hoffman <emmahoffman720@gmail.com>
2016-04-07 19:25:26 +0000

    Building User Profile Page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Nomster::Application.routes.draw do
   resources :places do
     resources :comments, only: :create
   end
+
+  resources :user, only: :shows
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   resources :places

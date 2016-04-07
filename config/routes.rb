1d556a15959a9aa828d75544b67545ea950bbe92
Emma Hoffman <emmahoffman720@gmail.com>
2016-04-07 19:28:50 +0000

    Solved error in Building user page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Nomster::Application.routes.draw do
     resources :comments, only: :create
   end
 
-  resources :user, only: :shows
+  resources :users, only: :show
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   resources :places
